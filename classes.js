1e338d24ab379c87fc9ffbc76f7b3399f83829f6
alex-saini <aaalexsainiii@gmail.com>
2018-07-19 14:32:01 +1200

    added third function 'purple'

--- classes.js
+++ classes.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one(),
-  two()
+  two(),
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -24,6 +25,10 @@ function two () {
   two.classList.add('green')
 }
 
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('purple')
+}
 // CREATE FUNCTION two HERE
 
 // CREATE FUNCTION three HERE
